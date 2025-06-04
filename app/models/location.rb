a82393ce05eb91116ddfdaea6577c02e2e5bfb03
Cherub Kumar <cherub.chopra@gmail.com>
2025-06-04 12:29:36 -0500

    Implement geocoding functionality in Location model and configure Geocoder settings for tests

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,55 @@
 class Location < ApplicationRecord
+  geocoded_by :address
+  before_validation :normalize_address
+  after_validation :geocode, if: :should_geocode?
+  after_validation :set_geocoded_attributes, if: :geocoded?
+
+  # Associations
+  has_one :forecast, dependent: :destroy
+
+  # Validations
+  validates :address, presence: true, uniqueness: { case_sensitive: false }
+  validate :coordinates_present_unless_skipped
+
+  attr_accessor :skip_geocoding
+
+  def display_name
+    return address if city.blank? && state.blank? && zipcode.blank?
+
+    [city, state, zipcode].reject(&:blank?).join(', ')
+  end
+
+  def geocoded?
+    latitude.present? && longitude.present?
+  end
+
+  def coordinates
+    [latitude, longitude]
+  end
+
+  private
+
+  def normalize_address
+    self.address = address&.strip&.squish
+  end
+
+  def should_geocode?
+    address_changed? && !skip_geocoding
+  end
+
+  def coordinates_present_unless_skipped
+    return if skip_geocoding || new_record?
+
+    errors.add(:base, "Location could not be geocoded") unless geocoded?
+  end
+
+  def set_geocoded_attributes
+    return unless geocoded?
+    return unless (result = Geocoder.search(coordinates).first)
+
+    self.city ||= result.city
+    self.state ||= result.state
+    self.zipcode ||= result.postal_code
+    self.country ||= result.country_code.upcase
+  end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Configure Geocoder for tests
+  Geocoder.configure(
+    timeout: 5,
+    lookup: :nominatim,
+    use_https: true
+  )
 end
