7aca06e5d3312e1d38986911f9c5cb32be14be45
Cherub Kumar <cherub.chopra@gmail.com>
2025-06-04 10:01:15 -0500

    Create location factory for tests

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,11 +1,30 @@
 FactoryBot.define do
   factory :location do
-    address { "MyString" }
-    city { "MyString" }
-    state { "MyString" }
-    zipcode { "MyString" }
-    country { "MyString" }
-    latitude { "9.99" }
-    longitude { "9.99" }
+    sequence(:address) { |n| "#{n} Main Street, Cupertino, CA 95014" }
+
+    city { "Cupertino" }
+    state { "CA" }
+    zipcode { "95014" }
+    country { "US" }
+    latitude { 37.331686 }
+    longitude { -122.030656 }
+
+    trait :not_geocoded do
+      latitude { nil }
+      longitude { nil }
+      city { nil }
+      state { nil }
+      zipcode { nil }
+      country { nil }
+    end
+
+    trait :san_francisco do
+      sequence(:address) { |n| "#{n} Market Street, San Francisco, CA 94105" }
+      city { "San Francisco" }
+      state { "CA" }
+      zipcode { "94105" }
+      latitude { 37.794220 }
+      longitude { -122.395055 }
+    end
   end
 end
