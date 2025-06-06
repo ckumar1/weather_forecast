3c8f1aba502c94294d26fcf3fcae451e6703a38e
Cherub Kumar <cherub.chopra@gmail.com>
2025-06-05 19:54:24 -0500

    Enhance security by adding CSRF protection and setting security headers in ApplicationController; update filter parameters to include sensitive data filtering

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,8 @@
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  # Weather API specific sensitive data
+  :api_key, :weather_api_key, :key,
+  # General security
+  :auth, :session, :csrf
 ]
